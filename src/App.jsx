1ef3585b209810783f3f0027d5ff555b2466cfa5
nozomi-iida <iida19990106@gmail.com>
2021-05-25 23:41:56 +0900

    fix: App.jsxを初期化

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
